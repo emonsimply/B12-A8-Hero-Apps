5487075a4cafaf5be22015b41703fa62b93a33d2
emonsimply <emonsimply@gmail.com>
2025-10-07 21:54:33 +0600

    Footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React from "react";
+import { FaFacebook, FaLinkedin, FaYoutube } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import logo from "../../assets/logo.png";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="bg-[#001931] text-gray-300 p-6 lg:p-10">
+      <div className="max-w-7xl mx-auto">
+        <div className="text-center">
+          <div className="flex justify-center items-center">
+            <img className="w-10" src={logo} alt="" />
+            <h2>HERO APPS</h2>
+          </div>
+          <p className="mt-3 text-sm max-w-xl mx-auto">
+            Hero Apps is your one-stop solution for discovering and installing
+            the best applications to enhance your productivity and lifestyle.
+          </p>
+        </div>
+
+        <div className="my-4 border opacity-50 border-dashed border-teal-600"></div>
+        {/* Links */}
+
+        <div className="flex justify-center gap-4 text-lg">
+          <a
+            href="#"
+            className="bg-sky-600 p-1 rounded-full text-white hover:scale-110 transition"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="#"
+            className="bg-white text-black p-1 rounded-full hover:scale-110 transition"
+          >
+            <FaXTwitter />
+          </a>
+          <a
+            href="#"
+            className="bg-blue-500 p-1 rounded-full text-white hover:scale-110 transition"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            href="#"
+            className="bg-red-600 p-1 rounded-full text-white hover:scale-110 transition"
+          >
+            <FaYoutube />
+          </a>
+        </div>
+
+        <div className="my-4 border opacity-50 border-dashed border-teal-600"></div>
+        {/* Social icons */}
+
+        <p className="text-center mt-8 text-sm opacity-50">
+          Copyright © 2025 - All right reserved
+        </p>
+      </div>
+    </footer>
   );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,40 +1,69 @@
-import React from 'react';
-import './Navbar.css'
-import { NavLink } from 'react-router';
+import React from "react";
+import "./Navbar.css";
+import { NavLink } from "react-router";
 
-import logo from '../../assets/logo.png'
-import { FaGithub } from 'react-icons/fa';
+import logo from "../../assets/logo.png";
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
-  const navLinks = <>
-  <li><NavLink>Home</NavLink></li>
-  <li><NavLink>Apps</NavLink></li>
-  <li><NavLink>Installation</NavLink></li>
-  </>
+  const navLinks = (
+    <>
+      <li>
+        <NavLink>Home</NavLink>
+      </li>
+      <li>
+        <NavLink>Apps</NavLink>
+      </li>
+      <li>
+        <NavLink>Installation</NavLink>
+      </li>
+    </>
+  );
   return (
     <div className="navbar bg-white shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      <div className="max-w-7xl mx-auto flex justify-between w-full">
+        <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {navLinks}
+          </ul>
+        </div>
+        <a className="btn btn-ghost font-bold bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent">
+          <img className="w-8" src={logo} alt="" />
+          HERO APPS
+        </a>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        {navLinks}
-      </ul>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn text-white bg-gradient-to-r from-[#632EE3] to-[#9F62F2]">
+          <FaGithub />
+          Contribution
+        </a>
+      </div>
+      </div>
     </div>
-    <a className="btn btn-ghost font-bold bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent"><img className='w-8' src={logo} alt="" />HERO APPS</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      {navLinks}
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn text-white bg-gradient-to-r from-[#632EE3] to-[#9F62F2]"><FaGithub />Contribution</a>
-  </div>
-</div>
   );
 };
 
